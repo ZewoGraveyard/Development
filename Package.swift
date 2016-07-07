8c4d6328b3a385bfc3081c00d34a3db43d87ce84
Dan Appel <dan.appel00@gmail.com>
2016-07-07 00:06:46 -0700

    Inline targets

--- Package.swift
+++ Package.swift
@@ -3,13 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Quark",
     targets: [
-        Target(
-            name: "Quark"
-        ),
-        Target(
-            name: "TestApplication",
-            dependencies: ["Quark"]
-        ),
+        Target(name: "Quark"),
+        Target(name: "TestApplication", dependencies: ["Quark"]),
     ],
     dependencies: [
         .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 10),
