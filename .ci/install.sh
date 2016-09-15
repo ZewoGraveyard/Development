5f38f61fe09bdb72e175991ad4b2bca193509364
Paulo Faria <paulo.faria.rl@gmail.com>
2016-09-15 05:37:38 -0300

    skip installing Swift 3 on macOS

--- .ci/install.sh
+++ .ci/install.sh
@@ -5,12 +5,14 @@
 set -e
 set -o pipefail
 
-git clone --depth 1 https://github.com/kylef/swiftenv.git ~/.swiftenv
-export SWIFTENV_ROOT="$HOME/.swiftenv"
-export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    git clone --depth 1 https://github.com/kylef/swiftenv.git ~/.swiftenv
+    export SWIFTENV_ROOT="$HOME/.swiftenv"
+    export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"
 
-if [ -f ".swift-version" ] || [ -n "$SWIFT_VERSION" ]; then
-  swiftenv install
-else
-  swiftenv rehash
+    if [ -f ".swift-version" ] || [ -n "$SWIFT_VERSION" ]; then
+      swiftenv install
+    else
+      swiftenv rehash
+    fi
 fi
