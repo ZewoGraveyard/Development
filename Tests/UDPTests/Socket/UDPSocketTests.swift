7e5e0e438c4d4bdc554e87f89905f7c102cebe0b
Romain Bossart <romain.bossart@fastmail.com>
2016-09-16 19:36:55 +0200

    [review] style and func signatures

--- Tests/UDPTests/Socket/UDPSocketTests.swift
+++ Tests/UDPTests/Socket/UDPSocketTests.swift
@@ -2,69 +2,45 @@ import XCTest
 @testable import Venice
 @testable import UDP
 
-public class UDPSocketTests: XCTestCase {
+public class UDPSocketTests : XCTestCase {
 
-    func testBasicSendReceive() {
-        let udpSocket = try? UDPSocket(ip: IP(port: 5050))
-        do {
-            try udpSocket?.send(Data("Hello world"), ip: IP(port: 5051))
-        }
-        catch {
-            XCTFail("failed to send data to the server")
-        }
-        XCTAssert(1 == 1, "Something is severely wrong here.")
-    }
-
-    func testBasicEcho() {
-
-        guard let serverSocket = try? UDPSocket(ip: IP(port: 5050)) else {
-            XCTFail("could not start listening socket")
-            return
-        }
-        guard let clientSocket = try? UDPSocket(ip: IP(port: 5051)) else {
-            XCTFail("could not start client socket")
-            return
-        }
+    func testBasicEcho() throws {
+        let serverSocket = try UDPSocket(ip: IP(port: 5050))
+        let clientSocket = try UDPSocket(ip: IP(port: 5051))
 
         let originalMessage = "Hello, World!"
         let comparisonChannel = Channel<Bool>()
 
         // Coroutine waiting for input message
         co {
-            guard let (data, _) = try? serverSocket.receive(upTo: 1024) else {
-                comparisonChannel.send(false)
-                return
+            var data = Data(count: 1024)
+            guard let (count, _) = try? serverSocket.read(into: &data, length: data.count) else {
+                return comparisonChannel.send(false)
             }
             // compare the received message and send back the comparison value
-            guard let message = String(data: data, encoding: String.Encoding.utf8) else {
-                comparisonChannel.send(false)
-                return
+            guard let receivedMessage = String(data: Data(data.prefix(count)), encoding: String.Encoding.utf8) else {
+                return comparisonChannel.send(false)
             }
-            comparisonChannel.send(message == originalMessage)
+            comparisonChannel.send(receivedMessage == originalMessage)
         }
 
         // Send data to the server
         do {
-            try clientSocket.send(Data(originalMessage), ip: IP(port: 5050))
-        }
-        catch {
+            try clientSocket.write(Data(originalMessage), to: IP(port: 5050))
+        } catch {
             XCTFail("failed to send data to the server")
             return
         }
         // Data was sent to the server
 
         let success = comparisonChannel.receive()!
-        // Received the comparison result
         XCTAssert(success, "Sent and received messages are not equal")
     }
 
-}
-
-extension UDPSocketTests {
     public static var allTests : [(String, (UDPSocketTests) -> () throws -> Void)] {
         return [
-           ("testBasicSendReceive", testBasicSendReceive),
            ("testBasicEcho", testBasicEcho),
         ]
     }
+
 }
