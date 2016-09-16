7d9dca0aca7e8dacd7551830b1b2e52c8a44a3d6
Paulo Faria <paulo.faria.rl@gmail.com>
2016-09-16 20:20:23 -0300

    remove HTTPFile

--- Modules/HTTPFile/Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "HTTPFile",
-    dependencies: [
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 13),
-        .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 13),
-    ]
-)
